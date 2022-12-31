84f40f0f91d0dd5bb45735987f15e3f77fbe41bb
= <=>
2022-12-31 15:41:15 +0530

    add

---
+++ index.js
@@ -0,0 +1,35 @@
+// var sum=(a,b)=>{
+//     add=a+b
+//     console.log(add);
+// }
+// sum(12,2)
+// var sum=a=>console.log(a)
+// sum(10)
+
+// setTimeout(() => {
+//     console.log("1");
+//     setTimeout(() => {
+//         console.log("2");
+//          setTimeout(() => {
+//             console.log("3");
+//          }, 3000);
+//     }, 2000);
+// }, 1000);
+
+//What is self-invoking function in JavaScript?
+// (function name(x){
+//     console.log(x);
+
+// }(10))
+
+
+
+// set timeout 
+
+// setTimeout(function sum(a,b){
+//     console.log(a*b);
+// },3000)
+
+
+// var a=10;
+// console.log(a);
